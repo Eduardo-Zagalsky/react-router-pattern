6231482db1f9a0022a17e19cef9bdbe8b4ff52ba
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-07-14 01:55:59 -0500

    added the color form to get the color name and value

--- Color/src/newColor.js
+++ Color/src/newColor.js
@@ -1,4 +1,29 @@
-const newColor = () => {
+import { useState } from "react";
+const INITIAL_VAL = { name: "", color: "" };
 
+const NewColor = ({ addColor }) => {
+    const [formData, setFormData] = useState(INITIAL_VAL);
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(data => ({ ...data, [name]: value }))
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addColor({ ...formData });
+        setFormData(INTIAL_VAL);
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="colorName">Color Name: </label>
+                <input type="text" name="colorName" value={formData.name} onChange={handleChange} />
+
+                <label htmlFor="color">Color: </label>
+                <input type="color" name="color" value={formData.name} onChange={handleChange} />
+
+                <button>Make Color</button>
+            </form>
+        </div>
+    )
 }
-export default newColor;
+export default NewColor;
