a9ff1e1e2c6e5944b24d482a8446426d62dbd7fc
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-07-14 01:57:07 -0500

    created the app with routes and form data.

--- Color/src/index.js
+++ Color/src/index.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Colors from './colors';
 import Color from './color';
-import newColor from './newColor';
+import NewColor from './newColor';
+import { useState } from 'react';
 
-const App = (
-  <BrowserRouter>
-    <Route path='/colors' >
-      <Colors />
-    </Route>
-    <Route path='/colors/:color' >
-      <Color />
-    </Route>
-    <Route path='/colors/new' >
-      <newColor />
-    </Route>
-  </BrowserRouter>
-)
+const App = () => {
+  const INITIAL_COLOR = { red: "000", green: "000", blue: "000" };
+  const [colors, setColors] = useState(INITIAL_COLOR);
+  const addColor = (formData) => {
+    setColors(color => [...color, { ...formData }])
+  }
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/colors' >
+          <Colors colors={colors} />
+        </Route>
+        <Route path='/colors/new' >
+          <NewColor addColor={addColor} />
+        </Route>
+        <Route path='/colors/:color' >
+          <Color colors={colors} />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
