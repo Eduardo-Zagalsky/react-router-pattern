5bedfbbfb280ecfe8acbc0b00f3262cc42559488
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-07-14 01:55:15 -0500

    added the links to the colors

--- Color/src/colors.js
+++ Color/src/colors.js
@@ -1,4 +1,13 @@
-const Colors = () => {
-
+import { Link } from "react-router-dom";
+const Colors = ({ colors }) => {
+    return (
+        <div>
+            {colors.map(c => {
+                <div>
+                    <Link to={`colors/${c.name}`}>{c.name}</Link>
+                </div>
+            })}
+        </div>
+    )
 }
 export default Colors;
