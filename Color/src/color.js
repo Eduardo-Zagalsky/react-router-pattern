3713e3fe6553f8d20d65b40b5ceda5fdd5705697
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-07-14 01:24:29 -0500

    added the individual color page

--- Color/src/color.js
+++ Color/src/color.js
@@ -1,4 +1,14 @@
-const Color = () => {
+import { useParams } from "react-router-dom";
+import { useState } from "react";
 
+const Color = ({ colors }) => {
+    const { color } = useParams();
+    const [currColor, setCurrColor] = useState("");
+    setCurrColor(colors[[color]])
+    return (
+        <div style={{ backgroundColor: currColor }}>
+            <h1>Do you like the color {color}</h1>
+        </div>
+    )
 }
 export default Color;
